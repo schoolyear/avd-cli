94b261d174f3794c6e2020f8a17379fdcbe3e311
Laurens Bremers <laurens@bremersklaver.nl>
2025-01-13 16:20:27 +0100

    support Windows self-updating

--- commands/update.go
+++ commands/update.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/c-bata/go-prompt"
@@ -42,8 +43,6 @@ var UpdateCommand = &cli.Command{
 			return errors.Wrap(err, "could not get the path of the current executable")
 		}
 
-		tmpFile := execPath + ".tmp"
-
 		fmt.Println("Checking latest version...")
 		latestVersion, latestDownloadURL, err := lib.FetchLatestVersion(c.Context)
 		if err != nil {
@@ -65,7 +64,6 @@ var UpdateCommand = &cli.Command{
 		}
 
 		fmt.Printf("Download from:\t%s\n", latestDownloadURL)
-		fmt.Printf("Download to:\t%s\n", tmpFile)
 		fmt.Printf("Install to:\t%s\n", execPath)
 
 		if !yesFlag {
@@ -80,7 +78,7 @@ var UpdateCommand = &cli.Command{
 			}
 		}
 
-		if err := downloadUpdate(c.Context, latestDownloadURL, execPath, tmpFile); err != nil {
+		if err := downloadUpdate(c.Context, latestDownloadURL, execPath); err != nil {
 			return errors.Wrap(err, "failed to perform update")
 		}
 		fmt.Println("Update complete!")
@@ -92,20 +90,11 @@ var UpdateCommand = &cli.Command{
 	},
 }
 
-func downloadUpdate(ctx context.Context, downloadUrl, targetPath, tmpFilePath string) error {
+func downloadUpdate(ctx context.Context, downloadUrl, targetPath string) error {
 	originalFileInfo, err := os.Stat(targetPath)
 	if err != nil {
 		return errors.Wrap(err, "failed to get the file permissions of the current executable")
 	}
-
-	tmpFile, err := os.OpenFile(tmpFilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, originalFileInfo.Mode().Perm())
-	if err != nil {
-		return errors.Wrapf(err, "failed to create temp file in %s", tmpFilePath)
-	}
-	defer func() {
-		tmpFile.Close()
-		os.Remove(tmpFilePath)
-	}()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, downloadUrl, nil)
 	if err != nil {
@@ -127,14 +116,28 @@ func downloadUpdate(ctx context.Context, downloadUrl, targetPath, tmpFilePath st
 		"Downloading",
 	)
 
-	_, err = io.Copy(io.MultiWriter(tmpFile, downloadProgress), resp.Body)
+	buffer := make([]byte, resp.ContentLength)
+	downloadBuffer := bytes.NewBuffer(buffer)
+
+	_, err = io.Copy(io.MultiWriter(downloadBuffer, downloadProgress), resp.Body)
 	if err != nil {
 		return errors.Wrap(err, "failed to download and write update to disk")
 	}
 
-	err = os.Rename(tmpFile.Name(), targetPath)
+	targetPathOld := targetPath + ".old"
+	if err := os.Rename(targetPath, targetPathOld); err != nil {
+		return errors.Wrap(err, "failed to rename the current executable")
+	}
+
+	targetFile, err := os.OpenFile(targetPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, originalFileInfo.Mode().Perm())
 	if err != nil {
-		return errors.Wrap(err, "failed to replace current version with newly downloaded version")
+		return errors.Wrap(err, "failed to open the executable for writing")
+	}
+	defer targetFile.Close()
+
+	_, err = io.Copy(targetFile, downloadBuffer)
+	if err != nil {
+		return errors.Wrap(err, "failed to write update to disk")
 	}
 
 	return nil
