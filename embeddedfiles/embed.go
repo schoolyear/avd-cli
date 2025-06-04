e2e387a42f6d2984c98359dec944a572b09167ec
Laurens Bremers <laurens@bremersklaver.nl>
2025-06-04 16:11:15 +0200

    embed

--- embeddedfiles/embed.go
+++ embeddedfiles/embed.go
@@ -6,3 +6,8 @@ import "embed"
 var ImageTemplate embed.FS
 
 const ImageTemplateBasePath = "image_template"
+
+//go:embed v2_image_template/*
+var V2ImageTemplate embed.FS
+
+const V2ImageTemplateBasePath = "v2_image_template"
