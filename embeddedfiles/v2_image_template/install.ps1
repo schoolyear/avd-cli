7039520c2e3fbaf77873394c8ce108733611a1ff
Laurens Bremers <laurens@bremersklaver.nl>
2025-06-04 16:10:07 +0200

    image layer template

---
+++ embeddedfiles/v2_image_template/install.ps1
@@ -0,0 +1,8 @@
+# This script is executed during the preparation of the exam image
+# This script is executed before the sysprep step
+#
+# This script is executed in its own layer folder
+# So, any file in this image layer, is available in the current working directory
+#
+# Once all installation scripts are executed, all image layer files are deleted
+# If you want to persist a file in the image, you must copy it to another folder
---
+++ embeddedfiles/v2_image_template/on_sessionhost_setup.ps1
@@ -0,0 +1,2 @@
+# This script is executed on each sessionhost during deployment
+# Note that any time spent in this script adds to the deployment time of each VM (and thus exam)
