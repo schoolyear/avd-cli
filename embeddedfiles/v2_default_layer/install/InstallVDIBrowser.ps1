3075692b40d2cad1e4466f14132d510817d3be51
Laurens Bremers <laurens@bremersklaver.nl>
2025-07-02 12:09:48 +0200

    revert back to old MSI

--- embeddedfiles/v2_default_layer/install/InstallVDIBrowser.ps1
+++ embeddedfiles/v2_default_layer/install/InstallVDIBrowser.ps1
@@ -16,6 +16,28 @@ $environmentUrls = @{
 
 $vdiBrowserUrl = $environmentUrls[$environment]
 
+function Exe-Download-Install {
+    param (
+        [string]$ExeUrl,
+        [string]$Name,
+        [string]$LocalFilePath
+    )
+
+    Write-Host "Downloading $Name EXE"
+    Invoke-WebRequest $ExeUrl -OutFile $LocalFilePath
+    Write-Host "Download of $Name completed"
+
+    Write-Host "Installing $Name"
+
+    $main_process = Start-Process -Wait -FilePath $LocalFilePath -ArgumentList "/install","/quiet" -PassThru
+    Write-Host "Installation of $Name complete"
+
+    Write-Host "Removing $Name EXE"
+    Remove-Item "$LocalFilePath"
+
+    return $main_process.ExitCode
+}
+
 function Msi-Download-Install {
     param (
         [string]$MsiUrl,
@@ -57,12 +79,48 @@ function Msi-Download-Install {
     return $main_exit_code
 }
 
+
+Write-Host "Download & install .NET"
+$exeOut = Exe-Download-Install -ExeUrl "https://download.visualstudio.microsoft.com/download/pr/4cb113f7-9553-4a2b-9c13-cd4fbd0cea30/02da5b68097af3c33b1b4ee5842f327e/dotnet-runtime-6.0.31-win-x86.exe" -Name ".NET runtime" -LocalFilePath "C:\netruntime.exe"
+if ($exeOut -ne 0)
+{
+    Write-Error "Exiting after .NET installation, because exit code is not 0, but $exeOut"
+    exit $exeOut
+}
+
+Write-Host "Download & install .NET Desktop"
+$exeOut = Exe-Download-Install -ExeUrl "https://download.visualstudio.microsoft.com/download/pr/b5fbd3de-7a12-43ba-b460-2f938fd802c3/627f6335ef3ba17bd3ef901c790d7575/windowsdesktop-runtime-6.0.31-win-x86.exe" -Name ".NET desktop runtime" -LocalFilePath "C:\netdesktopruntime.exe"
+if ($exeOut -ne 0)
+{
+    Write-Error "Exiting after .NET Desktop installation, because exit code is not 0, but $exeOut"
+    exit $exeOut
+}
+
 Write-Host "Download & install VDI browser"
 $msiOut = Msi-Download-Install -MsiUrl $vdiBrowserUrl -Name "VDI Browser" -LocalFilePath "C:\vdibrowser.msi" -MsiArguments 'VDIPROVIDER="avd"'
 if ($msiOut -ne 0)
 {
     Write-Error "Exiting after VDI Browser, because exit code is not 0, but $msiOut"
     exit $msiOut
+}
+
+Write-Host "Whitelisting VDI browser in the firewall"
+$syFolder = "C:\Program Files (x86)\Schoolyear\"
+$installationFolders = @("Schoolyear Browser Development (confidential)", "Schoolyear Browser Testing", "Schoolyear Browser Preview", "Schoolyear Browser")
+
+foreach ($folder in $installationFolders) {
+    $syExamsFolder= Join-Path -Path $syFolder -ChildPath $folder
+
+    $syExamsExe = "$syExamsFolder\shell\Schoolyear Exams.exe"
+    $syVDIServiceExe = "$syExamsFolder\schoolyear-vdi-service.exe"
+    $avdTokenProviderExe = "$syExamsFolder\shell\resources\avd_token_provider.exe"
+
+    # Allow the VDI browser
+    New-NetFirewallRule -DisplayName "Allow Schoolyear Browser outbound" -Program $syExamsExe -Direction Outbound -Action Allow -Profile Any | Out-Null
+    # Allow the background service
+    New-NetFirewallRule -DisplayName "Allow Schoolyear VDI service" -Program $syVDIServiceExe -Direction Outbound -Action Allow -Profile Any | Out-Null
+    # Allow the token provider
+    New-NetFirewallRule -DisplayName "Allow AVD token provider" -Program $avdTokenProviderExe -Direction Outbound -Action Allow -Profile Any | Out-Null
 }
 
 Write-Host "VDI browser installation completed"
